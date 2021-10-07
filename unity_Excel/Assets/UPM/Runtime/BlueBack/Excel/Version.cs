236a2d4254f6dcfecc813e3334e3035a4e434467
blueback <blueback28@gmail.com>
2021-10-07 20:13:58 +0900

    README.md / 修正

--- unity_Excel/Assets/UPM/Runtime/BlueBack/Excel/Version.cs
+++ unity_Excel/Assets/UPM/Runtime/BlueBack/Excel/Version.cs
@@ -21,7 +21,7 @@ namespace BlueBack.Excel
 
 		/** packageversion
 		*/
-		public const string packageversion = "0.0.15";
+		public const string packageversion = "0.0.14";
 
 		/** GetPackageVersion
 		*/
